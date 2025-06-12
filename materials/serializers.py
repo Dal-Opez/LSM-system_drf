9b0cc6d626ab2167ac5b3ae2b30d53de829654b7
DalOpez <alex-grazhdankin@mail.ru>
2025-06-12 22:12:55 +0300

    Task 1. Added Serializer for model Course

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,12 +1,28 @@
+from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 from materials.models import Course, Lesson
 
 
 class CourseSerializer(ModelSerializer):
+    count_lessons_in_course = SerializerMethodField()
+
     class Meta:
         model = Course
         fields = '__all__'
 
+    def get_count_lessons_in_course(self, course):
+        return course.lesson_set.count()
+
+
+class CourseDetailSerializer(ModelSerializer):
+    count_lessons_in_course = SerializerMethodField()
+
+    def get_count_lessons_in_course(self, course):
+        return course.lesson_set.count()
+
+    class Meta:
+        model = Course
+        fields = ('name', "count_lessons_in_course",)
 
 class LessonSerializer(ModelSerializer):
     class Meta:
