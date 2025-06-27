4e7c51cac2d93906d93e9038b754825e8b635631
DalOpez <alex-grazhdankin@mail.ru>
2025-06-28 00:43:07 +0300

    task 3. added user deactivating

--- config/celery.py
+++ config/celery.py
@@ -1,11 +1,18 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 app = Celery("config")
 app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks(['materials'])
+app.autodiscover_tasks(['materials', 'users'])
+app.conf.beat_schedule = {
+    'block-inactive-users-daily': {
+        'task': 'users.tasks.check_inactive_users',
+        'schedule': crontab(hour=0, minute=0),
+    },
+}
 app.conf.task_default_queue = 'celery'
 app.conf.task_routes = {
     'materials.tasks.*': {'queue': 'celery'}
